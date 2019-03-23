ad682b568d26f472a89a73c83ecb9e0078559c85
anandroid04 <48849520+anandroid04@users.noreply.github.com>
2019-03-23 10:05:16 +0530

    Create evenOdd.py

---
+++ evenOdd.py
@@ -0,0 +1,14 @@
+def check():
+    try: 
+        a=int(input())
+        if(a>0):
+            if(a%2==0):
+                print("Even")
+            else:
+                print("Odd")
+        else:
+            print("invalid")
+    except ValueError:
+            print("invalid")
+check()
+

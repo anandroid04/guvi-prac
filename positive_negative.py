86bfdff017bee20a08351add591ed83aa93c11ae
anandroid04 <48849520+anandroid04@users.noreply.github.com>
2019-04-11 11:35:16 +0530

    Update positive_negative.py

--- positive_negative.py
+++ positive_negative.py
@@ -1,7 +1,13 @@
-n=int(input(" "))
-if n>0:
-    print("Positive")
-elif n==0:
-    print("Zero")
-else:
-    print("Negative")
+
+def check():
+    try:
+        n=int(input())
+           if n>0:
+                print("Positive")
+            elif n==0:
+                print("Zero")
+            else:
+                print("Negative")
+       exccept ValueError:
+         print("Invalid")
+check()
